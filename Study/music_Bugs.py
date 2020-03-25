96ff60f3a628dd43781acf7edf77dcd55f41c080
binhk1004 <binhk1004@naver.com>
2020-03-25 22:48:41 +0900

    Crawling
    크롤링 예습 및 연습
    
    2020. 03. 25

---
+++ Study/music_Bugs.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://music.bugs.co.kr/chart',headers=headers)
+
+soup = BeautifulSoup(data.text, 'html.parser')
+
+music_info = soup.select('table.list.trackList.byChart > tbody > tr')
+rank = 0
+for music in music_info:
+    title_el = music.select('p.title')
+    singer_el = music.select('p.artist')
+    if len(title_el) > 0 :
+        rank += 1
+        title = title_el[0].text
+        singer = singer_el[0].text
+        print(rank,(title.replace('\n',"")),singer)
---
+++ Study/music_Genie.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20200309',headers=headers)
+
+soup = BeautifulSoup(data.text, 'html.parser')
+
+music_info = soup.select('table.list-wrap > tbody > tr')
+rank = 0
+for music in music_info:
+    title_el = music.select('a.title.ellipsis')
+    singer_el = music.select('a.artist.ellipsis')
+    if len(title_el) > 0 :
+        rank += 1
+        title = title_el[0].text
+        singer = singer_el[0].text
+        print(rank,(title.strip() + ''), (singer.strip() + ''))
+
+
+
+
